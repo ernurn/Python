1ac345d17519a604f22bebf915318143718b7a5e
Ernesto Nurnberg <evnzone@gmail.com>
2024-10-23 14:00:34 -0300

    Commit inicial

---
+++ 00_helloworld.py
@@ -0,0 +1,46 @@
+# First .py file
+
+# Exercise 2
+#2
+print("Hello World!")
+
+print(5 + 2) # addition(+)
+
+print(5 - 2) # subtraction(-)
+
+print(5 * 7) # multiplication(*)
+
+print(10 / 2) # division(/)
+
+print(2 ** 4) # exponential(**)
+
+print(13 // 2) # floor division operator(//)
+
+# 3
+print("Ernesto")
+print("Nurnberg")
+print("Argentina")
+print("I'm enjoying 30 days of python")
+
+# 4
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(14-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Ernesto"))
+print(type("Nurnberg"))
+print(type("Argentina"))
+
+# Exercise 3
+# 1
+print(10) # int
+print(4.5) # float
+print(1+1j) # complex
+print("Hello") #str
+print(True) #bool
+print(['Paraguay', 'Argentina', 'Brasil']) # list
+print((9.8, 3.14, 2.7)) #tuple
+print({9.8, 3.14, 2.7}) #set
+print({'name': 'Ernesto'}) #dict
