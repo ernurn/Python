2ab63f6e1d0eb4d60fdd962cf4e76b1c87777de2
Ernesto Nurnberg <evnzone@gmail.com>
2024-10-29 18:48:07 -0300

    Class 3 started

---
+++ 02_operators.py
@@ -0,0 +1,36 @@
+# Operators in Python
+
+# Arithmetic Operators
+
+print(5 + 2)    # add
+print(5 - 2)    # subtraction
+print(5 * 2)    # Multiplication
+print(5 / 2)    # Division
+print(5 % 2)    # Modulus
+print(5 // 2)   # Floor division
+print(5 ** 2)   # Exponentiation
+
+# Coparative Operators
+print(3 == 4)   # Equal
+print(3 != 4)   # Not equal
+print(3 > 4)    # Greater than
+print(3 < 4)    # Less than
+print(3 >= 4)   # Greater than or equal to
+print(3 <= 4)   # Less than or equal to
+
+print("Hello" == "Python")   # Equal
+print("Hello" != "Python")   # Not equal
+print("Hello" > "Python")    # Greater than
+print("Hello" < "Python")    # Less than
+print("Hello">= "Python")   # Greater than or equal to
+print("Hello"<= "Python")   # Less than or equal to
+
+# Logics Operators
+print(3 > 4 and 4 == 4)     # and is TRUE if both are TRUE
+print(3 > 4 or 4 == 4)      # or is TRUE if one of them is TRUE
+
+
+# Strings Operators
+print('Hello' + 'Python')   # Concatenate
+
+
